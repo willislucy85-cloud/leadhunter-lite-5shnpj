61a55fa607a1ca0759c2537212cd677de879211d
willislucy85-cloud <willis.lucy85@gmail.com>
2026-07-17 03:38:51 +0000

    Fix TypeScript/Next config and add typecheck script

---
+++ next-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
